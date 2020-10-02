1040b6cbd34f1950268647858a743a04e2830001
MunibaWq <muniba.wq@gmail.com>
2020-10-02 10:25:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-script
+    //Animation init
+    new WOW().init();
+
+    //Modal
+    $('#myModal').on('shown.bs.modal', function () {
+      $('#myInput').focus()
+    })
+
+    // Material Select Initialization
+    $(document).ready(function () {
+      $('.mdb-select').material_select();
+    });
+
+    // MDB Lightbox Init
+    $(function () {
+      $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+    });
